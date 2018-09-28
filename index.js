65d9b35094c5c6f044244dfbbe346389b72e5195
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-09-28 16:30:40 +0900

    expose async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -7,41 +7,35 @@
 const fettuccine = require('fettuccine');
 const decompressTargz = require('decompress-targz');
 
-function decompressResponseBody(response) {
-  if (response.statusCode < 200 || 299 < response.statusCode) {
-    return Promise.reject(new Error(`${response.statusCode} ${response.statusMessage}`));
-  }
-
-  return decompressTargz()(response.body);
+function isZoneTab({path}) {
+  return path === 'zone.tab';
 }
 
-function isZoneTab(file) {
-  return file.path === 'zone.tab';
-}
-
-module.exports = function getZonetab(...args) {
+module.exports = async function getZonetab(...args) {
   const argLen = args.length;
 
   if (argLen > 1) {
-    return Promise.reject(new RangeError(`Expected 0 or 1 argument (<Object>), but got ${argLen} arguments.`));
+    throw new RangeError(`Expected 0 or 1 argument (<Object>), but got ${argLen} arguments.`);
   }
 
-  const options = args[0];
+  const [options] = args;
 
-  return fettuccine('time-zones/repository/tzdata-latest.tar.gz', Object.assign({
+  const response = await fettuccine('time-zones/repository/tzdata-latest.tar.gz', Object.assign({
     baseUrl: 'https://www.iana.org/'
   }, options, {
     encoding: null
-  }))
-  .then(decompressResponseBody)
-  .then(function extractZoneTabData(files) {
-    const data = files.find(isZoneTab).data;
-    const encoding = Object.assign({encoding: 'utf8'}, options).encoding;
+  }));
 
-    if (encoding !== null) {
-      return data.toString(encoding);
-    }
+  if (response.statusCode < 200 || 299 < response.statusCode) {
+    throw new Error(`${response.statusCode} ${response.statusMessage}`);
+  }
 
-    return data;
-  });
+  const data = (await decompressTargz()(response.body)).find(isZoneTab).data;
+  const encoding = Object.assign({encoding: 'utf8'}, options).encoding;
+
+  if (encoding !== null) {
+    return data.toString(encoding);
+  }
+
+  return data;
 };
