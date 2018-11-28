71172de76ddddacdc19cb8756d3b64511a10df0a
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-11-28 12:27:31 +0900

    use async/await in the test script

--- test.js
+++ test.js
@@ -1,49 +1,54 @@
-/* eslint-disable promise/prefer-await-to-then */
 'use strict';
 
 const getZonetab = require('.');
 const test = require('tape');
 
-test('getZonetab()', t => {
-	t.plan(6);
+test('getZonetab()', async t => {
+	t.plan(5);
 
-	t.strictEqual(getZonetab.name, 'getZonetab', 'should have a function name.');
+	(async () => {
+		const data = await getZonetab();
 
-	getZonetab().then(data => {
-		t.strictEqual(
-			typeof data === 'string',
-			true,
+		t.equal(
+			typeof data,
+			'string',
 			'should be fulfilled with a string.'
 		);
 
-		t.strictEqual(
-			data.indexOf('# tz zone descriptions (deprecated version)'),
+		t.equal(
+			data.indexOf('# tzdb timezone descriptions (deprecated version)'),
 			0,
 			'should be fulfilled with a zone.tab data.'
 		);
-	}).catch(t.fail);
+	})();
 
-	getZonetab({encoding: null}).then(data => {
-		t.strictEqual(
-			Buffer.isBuffer(data),
+	(async () => {
+		t.equal(
+			Buffer.isBuffer(await getZonetab({encoding: null})),
 			true,
 			'should be fulfilled with a buffer when `encoding` option is null.'
 		);
-	}).catch(t.fail);
+	})();
 
-	getZonetab({baseUrl: 'https://example.org'}).then(t.fail, err => {
-		t.strictEqual(
-			err.message,
-			'404 Not Found',
-			'should fail when it cannot get zone.tab file.'
-		);
-	}).catch(t.fail);
+	(async () => {
+		try {
+			await getZonetab({baseUrl: 'https://example.org'});
+		} catch ({message}) {
+			t.equal(
+				message,
+				'404 Not Found',
+				'should fail when it cannot get zone.tab file.'
+			);
+		}
+	})();
 
-	getZonetab({}, {}).then(t.fail, err => {
-		t.strictEqual(
+	try {
+		await getZonetab({}, {});
+	} catch (err) {
+		t.equal(
 			err.toString(),
 			'RangeError: Expected 0 or 1 argument (<Object>), but got 2 arguments.',
 			'should fail when it takes too many arguments.'
 		);
-	}).catch(t.fail);
+	}
 });
